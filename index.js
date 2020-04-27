5879bfd2ed8fc30d3db332b520b3a6039be023b0
Joe <josephedwardwork@gmail.com>
2020-04-26 20:37:06 -0400

    update contact form

--- index.js
+++ index.js
@@ -1,60 +1,79 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('./'))
+app.use(express.static("./"));
 const path = require("path");
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-app.use(express.json()) // for parsing application/json
+app.use(express.json());
 app.use(express.urlencoded({ extended: true })) /
+  app.get("/", function (req, res) {
+    res.sendFile("index.html", { root: path.join(__dirname, "./") });
+  });
 
-app.get('/', function(req,res){
-    res.sendFile('index.html', { root: path.join(__dirname, './') });
-});
-
-app.post('/contact', function(req, res) {
-
+app.post("/contact", function (req, res) {
   req.body.name;
 
   var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'thekittrellcompany@gmail.com',
-      pass: process.env.test
-    }
+      user: "thekittrellcompany@gmail.com",
+      pass: process.env.test,
+    },
   });
-  
 
   var mailOptions = {
-    from: 'thekittrellcompany@gmail.com',
+    from: "thekittrellcompany@gmail.com",
     to: req.body.email,
-    subject: 'Howdy from The Kittrell Company!',
-    text: "Hello "+req.body.name+"!"+'\n\n'+
-    "Glad you like our houses. "+
-    "Please don't hesitate to ask any questions here. "+
-    "We are quite busy, but if you would like to discuss buying a home, "+
-    "David will contact you to set up a discussion at his earliest convenience."+
-     "\n\n"+"Best, "+"\n"+"The Kittrell Company"
+    subject: "Howdy from The Kittrell Company!",
+    text:
+      "Hello " +
+      req.body.name +
+      "!" +
+      "\n\n" +
+      "Glad you like our houses. " +
+      "Please don't hesitate to ask any questions here. " +
+      "We are quite busy, but if you would like to discuss buying a home, " +
+      "David will contact you to set up a discussion at his earliest convenience." +
+      "\n\n" +
+      "Best, " +
+      "\n" +
+      "The Kittrell Company",
   };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-   
+
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Contact Email sent: " + info.response);
     }
-
   });
 
-  //create transporter for sending contact details to us?
+  //create second transporter for sending contact details to us
+  var contactOptions = {
+    from: "thekittrellcompany.com website",
+    to: "thekittrellcompany@gmail.com",
+    subject: "Contact Submission from Website",
+    text: 'Name: '+req.body.name+'\n'
+     +'Phone: '+req.body.phone+'\n'
+     +'Email: '+req.body.email+'\n'
+     +'Message: '+req.body.message
+  };
+
+  transporter.sendMail(contactOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Admin Email sent: " + info.response);
+    }
+  });
+
+
+
 });
 
-
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
-  
-
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
--- js/validation.js
+++ js/validation.js
@@ -6,10 +6,10 @@
             
             // Variable declaration
             var error = false;
-            var name = $('#name').val();
-            var email = $('#email').val();
+            var name = $('#form_name').val();
+            var email = $('#form_email').val();
 			var phone = $('#phone').val();
-            var message = $('#message').val();
+            var message = $('#form_message').val();
 			
 			$('#name,#email,#phone,#message').click(function(){
 				$(this).removeClass("error_input");
@@ -45,22 +45,25 @@
             if(error == false){
                // Disable submit button just after the form processed 1st time successfully.
                 $('#send_message').attr({'disabled' : 'true', 'value' : 'Sending...' });
-                
-				/* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
-                $.post("email.php", $("#contact_form").serialize(),function(result){
-                    //Check the result set from email.php file.
-                    if(result == 'sent'){
-                        //If the email is sent successfully, remove the submit button
-                         $('#submit').remove();
-                        //Display the success message
-                        $('#mail_success').fadeIn(500);
-                    }else{
-                        //Display the error message
-                        $('#mail_fail').fadeIn(500);
-                        // Enable the submit button again
-                        $('#send_message').removeAttr('disabled').attr('value', 'Send The Message');
-                    }
-                });
+                $('#mail_sent').fadeIn(500);    
+                setTimeout(function () {
+                   window.location.reload(); 
+                }, 3000);            
+				// /* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
+                // $.post("email.php", $("#contact_form").serialize(),function(result){
+                //     //Check the result set from email.php file.
+                //     if(result == 'sent'){
+                //         //If the email is sent successfully, remove the submit button
+                //          $('#submit').remove();
+                //         //Display the success message
+                //         $('#mail_success').fadeIn(500);
+                //     }else{
+                //         //Display the error message
+                //         $('#mail_fail').fadeIn(500);
+                //         // Enable the submit button again
+                //         $('#send_message').removeAttr('disabled').attr('value', 'Send The Message');
+                //     }
+                // });
             }
         });    
     });
