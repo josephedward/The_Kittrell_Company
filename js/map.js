ab4cfe66be24e1702f21c5eba7ebab0678a7c942
Joe <josephedwardwork@gmail.com>
2020-05-09 23:33:30 -0400

    commit

--- js/map.js
+++ js/map.js
@@ -1,24 +1,25 @@
                     // When the window has finished loading create our google map below
-                    google.maps.event.addDomListener(window, 'load', init);
+                    // google.maps.event.addDomListener(window, 'load', init);
 
                     function init() {
                         // Basic options for a simple Google Map
                         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
                         var myLatlng = new google.maps.LatLng(37.5967007, -77.5162545);
 
+
                         var mapOptions = {
                             // How zoomed in you want the map to start at (always required)
-                            zoom: 15,
-                            disableDefaultUI: true,
-							scrollwheel: false, 
-
+                            zoom: 10,
+                            disableDefaultUI: false,
+							scrollwheel: true, 
+                            
                             // The latitude and longitude to center the map (always required)
 
                             center: myLatlng, // New York
 
                             // How you would like to style the map. 
                             // This is where you would paste any style found on Snazzy Maps.
-                            styles: [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
+                            // styles: [{"stylers":[{"hue":"#ff1a00"},{"invert_lightness":true},{"saturation":-100},{"lightness":33},{"gamma":0.5}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2D333C"}]}]
                         }; 
 
                         // Get the HTML DOM element that will contain your map 
@@ -27,14 +28,27 @@
 
                         // Create the Google Map using out element and options defined above
                         var map = new google.maps.Map(mapElement, mapOptions);
-						
+                        
+                        var infowindow = new google.maps.InfoWindow({
+                            content: "<span>The Kittrell Company</span>"+"<br/>"+
+                            "<span>6327 Mallory Drive</span>"+"<br/>"+
+                            "<span>Richmond, Virginia - 23226</span>"+"<br/>"+
+                            "<span><strong>Phone:</strong>804-285-7869</span>"+"<br/>"+
+                            "<span><strong>Email:</strong><a href='mailto:thekittrellcompany@gmail.com'>thekittrellcompany@gmail.com</a></span>"
+                          });
+                        
                         var marker = new google.maps.Marker({
                             position: myLatlng,
                             map: map,
-							icon: 'images/map-marker.png',
-                            title: 'The Kittrell Company'
+                            // label:'The Kittrell Company',
+                            // animation: google.maps.Animation.DROP,
+							// icon: 'images/map-marker.png',
+                            title: 'The Kittrell Company',
+                            
                         });
-						
+						// marker.addListener('click', function() {
+                            infowindow.open(map, marker);
+                        //   });
 						
 
                     }
